b6aed1bdafd9bdcb1733fb140221da5492eb6058
Sheed144 <119950644+Sheed144@users.noreply.github.com>
2022-12-10 12:31:07 +0100

    Abdulrasheed

---
+++ Final Project/Final project.js
@@ -0,0 +1,10 @@
+
+var fontSize = 1;
+function zoomIn() {
+	fontSize += 0.1;
+	document.body.style.fontSize = fontSize + "em";
+}
+function zoomOut() {
+	fontSize -= 0.1;
+	document.body.style.fontSize = fontSize + "em";
+}
